4af5a49b35ef9aa5e26fc5943eb25456f0901fd1
caoyi <mizhiejun520@gmail.com>
2026-03-08 22:09:39 +0800

    添加项目文件。

---
+++ Culinary Guide/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Culinary_Guide
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Culinary Guide/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Culinary_Guide
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
