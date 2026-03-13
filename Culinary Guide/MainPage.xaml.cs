965d2608c80e4041c3a3f2ece407802c1327e5cd
caoyi <mizhiejun520@gmail.com>
2026-03-13 21:11:47 +0800

    feat：xaml CollectionView 设置 SelectionMode="Single"，添加 SelectionChanged="OnRestaurantSelected"，移除 Border 上的 TapGestureRecognizer 和所有 InputTransparent

--- Culinary Guide/MainPage.xaml.cs
+++ Culinary Guide/MainPage.xaml.cs
@@ -110,23 +110,13 @@ namespace Culinary_Guide
             }
         }
 
-        private async void OnRestaurantClicked(object sender, EventArgs e)
+        private async void OnRestaurantSelected(object sender, SelectionChangedEventArgs e)
         {
-            Restaurant? restaurant = null;
-            
-            if (sender is TapGestureRecognizer tap)
-            {
-                restaurant = tap.BindingContext as Restaurant;
-            }
-            else if (sender is Image image)
-            {
-                restaurant = image.BindingContext as Restaurant;
-            }
-            
-            if (restaurant != null)
+            if (e.CurrentSelection.FirstOrDefault() is Restaurant restaurant)
             {
                 var detailPage = new RestaurantDetailPage(_restaurantService, _favoriteService, restaurant);
                 await Navigation.PushAsync(detailPage);
+                RestaurantList.SelectedItem = null;
             }
         }
     }
